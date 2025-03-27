bed8e73b717fca9c095184ca0cd0c7de227892a2
AndrewG12-hub <geisler02andrew@gmail.com>
2025-03-27 16:49:11 -0400

    Updated Sidebar with FitBit Login (3-27-25)

--- sidebar.js
+++ sidebar.js
@@ -5,10 +5,11 @@ import { useNavigate, Navigate} from "react-router-dom"; // Import useNavigate
 import 'bootstrap/dist/css/bootstrap.css'
 import { Container, Card, Form, Button} from "react-bootstrap";
 import FitbitSyncButton from './FitbitSyncButton';
+import FitbitLoginForm from './FitbitLoginForm';
 
 function DashboardSideBar(){
   return (
-    <Container className="d-flex flex-column align-items-start p-3" style={{margin:"0",width:"250px",height:"500px", backgroundColor:"#AFAFAF"}}>
+    <Container className="d-flex flex-column align-items-start p-3" style={{margin:"0",width:"250px",height:"600px", backgroundColor:"#AFAFAF"}}>
     
       <img alt="logo" src = "https://1000logos.net/wp-content/uploads/2018/01/Penn-State-Logo.png"
       style={{ height: '105px', width: '200px' }} />
@@ -24,13 +25,13 @@ function DashboardSideBar(){
       </div>
 
 
-      <div style={{padding:"10px",paddingTop:"200px"}}>
+      <div style = {{margin:"0% 10px 0% 0px"}}><FitbitLoginForm/></div>
+      
+      <div style={{margin:"90% 10px 0% 0px"}}><FitbitSyncButton/></div>
+      
+      <div style={{padding:"10px", margin:"-40% 10px 0% 0px"}}>
         <Button onClick={() => Navigate("/")}  type="submit" style = {{backgroundColor:"#7D7D7D", border:"none", width:"200px"}}>Logout</Button>
       </div>
-      
-      <div><FitbitSyncButton/></div>
-        
-      
 
     </Container>
 
